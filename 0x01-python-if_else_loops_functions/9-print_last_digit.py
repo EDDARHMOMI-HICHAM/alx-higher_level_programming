3a8e79ee1c147c4030187bc7ad2a5fa81a9f70c4
Hix.Mox <heddarhmoumigmail.com>
2023-11-28 18:33:36 +0100

    push task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = abs(number) % 10  # Get the absolute value to handle negative numbers
+    print("{}".format(last_digit), end="")
+    return last_digit
