ac64665efd7776d8f0a35de5116162bea5d829ac
Hix.Mox <heddarhmoumigmail.com>
2023-12-02 00:23:09 +0100

    push task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
+
+a = 1
+b = 2
 
 if __name__ == "__main__":
-    pass
+    from add_0 import add
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
