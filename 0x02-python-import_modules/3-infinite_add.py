8a6f8832b1730f5cde6b35fcbfe947f765c906c2
Hix.Mox <heddarhmoumigmail.com>
2023-12-02 01:13:42 +0100

    push task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,11 +2,8 @@
 from sys import argv
 
 if __name__ == "__main__":
+    arguments = argv[1:]
 
-arguments = argv[1:]
-result = 0
+    result = sum(int(arg) for arg in arguments)
 
-for arg in arguments:
-    result += int(arg)
-
-print(result)
+    print(result)
